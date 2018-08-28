b5b020a6d57d839c9ca9755978fb92b716ea1905
nick <nmdufour@gmail.com>
2018-08-28 19:55:58 -0400

    added slideshow with screencaps

--- main-comp.js
+++ main-comp.js
@@ -21,7 +21,79 @@ const main = {
         Check out some of my projects and <a href="#!/resume">resumé</a>, and feel free to reach out if
         you're interested in working together.  Thanks for visiting!
       </p>
-    </section>`
+    </section>
+
+    <section class="mob-slideshow">
+
+    <section class="prev" ng-hide="$ctrl.count === 0;">
+      <i class="material-icons md-36 prev" ng-click="$ctrl.goBack();">arrow_back_ios</i>
+    </section>
+
+      <section class="proj-img-container">
+        <a href="{{ $ctrl.mobileDisplay[$ctrl.count].link }}">
+          <img class="proj-img" ng-src="{{ $ctrl.mobileDisplay[$ctrl.count].image }}" alt="Screencap of a project I worked on">
+        </a>
+      </section>
+
+      <section class="next">
+        <i class="material-icons md-36 next" ng-click="$ctrl.goForward();">arrow_forward_ios</i>
+      </section>
+
+    </section>
+
+    <section class="desk-slideshow">
+
+    <section class="prev" ng-hide="$ctrl.count === 0;">
+      <i class="material-icons md-36 prev" ng-click="$ctrl.goBackDesk();">arrow_back_ios</i>
+    </section>
+
+      <section class="desk-img-container">
+        <a href="{{ $ctrl.deskDisplay[$ctrl.count].link }}">
+          <img class="desk-proj-img" ng-src="{{ $ctrl.deskDisplay[$ctrl.count].image }}">
+        </a>
+      </section>
+
+      <section class="next">
+        <i class="material-icons md-36 next" ng-click="$ctrl.goForwardDesk();">arrow_forward_ios</i>
+      </section>
+
+    </section>
+
+    `,
+    controller: ["PFService", function(PFService){
+      const vm = this;
+      //Images are imported from the service
+      vm.mobileDisplay = PFService.returnMobileImages();
+      vm.deskDisplay = PFService.returnDeskImages();
+      //Declare counter and functions to go back and forth in the slideshow
+      vm.count = 0;
+      vm.goForward = () => {
+        vm.count++;
+        console.log(vm.count);
+        if (vm.count > vm.mobileDisplay.length - 1) {
+          vm.count = 0;
+          console.log("reset");
+        }
+      }
+      vm.goBack = () => {
+        vm.count--;
+        console.log(vm.count);
+      }
+  // The mobile and desktop slidehows are different lengths so I made functions to handle both cases
+      vm.goForwardDesk = () => {
+        vm.count++;
+        console.log(vm.count);
+        if (vm.count > vm.deskDisplay.length - 1) {
+          vm.count = 0;
+          console.log("reset");
+        }
+      }
+      vm.goBackDesk = () => {
+        vm.count--;
+        console.log(vm.count);
+      }
+    }
+  ]
 }
 
 angular
