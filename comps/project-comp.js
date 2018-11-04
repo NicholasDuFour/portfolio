f5ab32c4a8bcba9b034d7725f7508768b584c3b7
nick <nmdufour@gmail.com>
2018-11-04 12:27:44 -0500

    fixed rawgit project links

--- comps/project-comp.js
+++ comps/project-comp.js
@@ -20,28 +20,28 @@ template: `
       <p>An art discovery app using the Harvard Art Museums API.</p>
       <h5>Made with HTML, CSS and AngularJS.</h5>
 
-    <a href="https://rawgit.com/iamkevinw/Midterm/master/index.html">
+    <a href="https://nicholasdufour.github.io/Midterm-WichHunt/">
       <h3>Sandwich Shop: 'Wich Hunt (made with team of three)</h3></a>
       <p>A point of sale site for a sandwich shop with a witch theme.</p>
       <h5>Made with HTML, CSS and jQuery.</h5>
 
-    <a href="https://rawgit.com/NicholasDuFour/angularproject/master/index.html">
+    <a href="https://nicholasdufour.github.io/angularproject/">
       <h3>Flick Finder (made with team of two)</h3></a>
       <p>A site to assist users with finding a movie to watch, importing data
         from TheMovieDB API.</p>
       <h5>Made with HTML, CSS and AngularJS.</h5>
 
-    <a href="https://rawgit.com/NicholasDuFour/todo-lab-p2/master/index.html">
+    <a href="https://nicholasdufour.github.io/todo-lab-p2/">
       <h3>To-Do App</h3></a>
       <p>An app allowing users to manage their to-do list.</p>
       <h5>Made with HTML, CSS and AngularJS.</h5>
 
-    <a href="https://rawgit.com/NicholasDuFour/jq-lab1-p2/master/jq-lab1-p2-index.html">
+    <a href=" https://nicholasdufour.github.io/jq-lab1-p2/">
       <h3>Table Finder</h3></a>
       <p>A simple app in which the user can reserve a table at a restaurant.</p>
       <h5>Made with HTML, CSS and jQuery.</h5>
 
-    <a href="https://rawgit.com/NicholasDuFour/js-lab1-p4/master/index-js-lab1-p4.html">
+    <a href="https://nicholasdufour.github.io/js-lab1-p4/">
         <h3>Address Book</h3></a>
       <p>A site enabling the user to add and delete contacts with a stylish layout.</p>
       <h5>Made with HTML, CSS and JavaScript.</h5>
